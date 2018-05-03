e748a99f6d3e4e98529a685412a4673d6ed9dd2d
HLKnight <nhatminh.nguyen.35175@gmail.com>
2018-05-03 01:19:52 +0000

    03.05.2018

--- setting.h
+++ setting.h
@@ -1,10 +1,24 @@
-// DEBUG mode
+/*
+	DEBUG mode:
+	Choose DEBUG mode to display information as text
+*/
 //#define DEBUG
-// Display mode
+/*
+	Display mode:
+*/
 #define UNICODE
-// Set the maximum number of network scanned
+/*
+	Set the maximum number of network scanned
+*/
 #define MAX 20
-// Set the signal level filter
+/*
+	Set the signal level filter:
+	Any thing below this level will not be displayed
+*/
 #define FILTER -80
-// Set the resolution (1 or 2)
+/*
+	Set the resolution:
+	Choose "1" to run on default putty screen size
+	Choose "2" to run on maximized putty screen
+*/
 #define RES 2
